5f8e313d0571ce8f21f451836e557b7e7e83784e
shahriar_hj <shahriarhajiabbasi@gmail.com>
2020-10-28 13:05:15 +0330

    start the program in IRIS data set

--- TAMRIN_1.R
+++ TAMRIN_1.R
@@ -1,22 +1,8 @@
-# tamrin R Based on 
-# "https://cran.r-project.org/doc/manuals/R-intro.html#SEC_Contents"
+### this code 
 
 
-y = c(1:10)
-y
-x <- c(10, 4, 6, 5.6, 10:20, 21.74)
-x
-v <- 2*x + y + 1
-v
-range(v)
-length(x)
-sqrt(-17)
-sqrt(-17+0i)
-1:30
-2*1:15
-n <- 10
-1:n-1
-1:(n-1)
-seq(40, 1, -5)
-seq(-5, 5, 0.2) -> s3
-s3
+rm(list = ls())      # clean the objects
+add.df <- function(a, b){
+  newdf <- cbind(a,b)
+  return(newdf)
+}
---
+++ workonIRIS.R
@@ -0,0 +1,7 @@
+# work on Iris Dataset
+library(datasets)
+library(RCurl)
+data("iris")
+iris <-  datasets::iris
+View(iris)
+
